573720d7bd9553698478b5864840eb2fce95d819
oca <codeconnects@gmail.com>
2022-12-28 12:23:04 -0500

    get canvas element and set basic size

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+    // put canvas reference in const
+    const canvas = document.getElementById('canvas1');
+
+    // context type, '2d' or 'webgl' (3d)
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+});
