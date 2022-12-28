ebdf938728902edaf77cea5454fb0e0ce07cf259
oca <codeconnects@gmail.com>
2022-12-28 12:50:13 -0500

    add animation loop, add game and player constructors

--- script.js
+++ script.js
@@ -17,7 +17,20 @@ window.addEventListener('load', function(){
 
     }
     class Player {
-
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = -0.5;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
 
@@ -32,7 +45,32 @@ window.addEventListener('load', function(){
 
     }
     class Game {
-        
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context);
+        }
     }
 
+    const game = new Game(canvas.width, canvas.height);
+
+    // animation loop
+    function animate(){
+
+        // clear what has already been drawn while looping
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        game.update();
+        game.draw(ctx);
+
+        // using parent as argument to make endless loop
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
