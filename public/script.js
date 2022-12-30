d34aee7f187a98c23b6e278d06c49b16eb796dd5
oca <codeconnects@gmail.com>
2022-12-30 16:28:07 -0500

    give enemies lives and add collision detection with projectiles

--- public/script.js
+++ public/script.js
@@ -92,6 +92,8 @@ window.addEventListener('load', function(){
             this.x = this.game.width;
             this.speedX = Math.random() * -1.5 - 0.5;
             this.markedForDeletion = false;
+            this.lives = 5;
+            this.score = this.lives;
         }
         update(){
             this.x += this.speedX;
@@ -100,6 +102,9 @@ window.addEventListener('load', function(){
         draw(context){
             context.fillStyle = 'red';
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle = 'black';
+            context.font = '20px Helvetica';
+            context.fillText(this.lives, this.x, this.y)
         }
     }
     class Angler1 extends Enemy {
@@ -166,6 +171,16 @@ window.addEventListener('load', function(){
                 if (this.checkCollision(this.player, enemy)){
                     enemy.markedForDeletion = true;
                 }
+                this.player.projectiles.forEach(projectile => {
+                    if (this.checkCollision(projectile, enemy)){
+                        enemy.lives--;
+                        projectile.markedForDeletion = true;
+                        if (enemy.lives <= 0){
+                            enemy.markedForDeletion = true;
+                            this.score += enemy.score;
+                        }
+                    }
+                });
             });
 
             this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
