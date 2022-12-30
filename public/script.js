6b9d8399c8ab042525c6c82c8cb76fc3833a4453
oca <codeconnects@gmail.com>
2022-12-30 16:19:13 -0500

    add collisions base

--- public/script.js
+++ public/script.js
@@ -163,6 +163,9 @@ window.addEventListener('load', function(){
             }
             this.enemies.forEach(enemy => {
                 enemy.update();
+                if (this.checkCollision(this.player, enemy)){
+                    enemy.markedForDeletion = true;
+                }
             });
 
             this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
@@ -183,6 +186,12 @@ window.addEventListener('load', function(){
         }
         addEnemy(){
             this.enemies.push(new Angler1(this));
+        }
+        checkCollision(rect1, rect2){
+            return (    rect1.x < rect2.x + rect2.width &&
+                        rect1.x + rect1.width > rect2.x &&
+                        rect1.y < rect2.y + rect2.height &&
+                        rect1.height + rect1.y > rect2.y)
         }
     }
 
