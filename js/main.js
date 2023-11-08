58d6eb79cb4b1a5bd367ffa541c9d3e45018233c
CodeConnects <codeconnects@gmail.com>
2023-11-08 13:03:36 -0500

    add start and reset functionality, plus change time tracking functions

--- js/main.js
+++ js/main.js
@@ -1,57 +1,51 @@
 import Game from './classes/Game.js';
 
 window.addEventListener('load', function () {
-  
-  // put canvas reference in const
   const canvas = document.getElementById('canvas1');
-
-  // context type, '2d' or 'webgl' (3d)
   const ctx = canvas.getContext('2d');
-  const w = 1240;
-  const h = 700;
+  const startScreen = document.getElementById('startScreen');
+  const gameOverScreen = document.getElementById('gameOverScreen');
+  const startButton = document.getElementById('startButton');
+  const restartButton = document.getElementById('restartButton');
 
-  canvas.width = w;
-  canvas.height = h;
+  canvas.width = 1240;
+  canvas.height = 700;
 
   const game = new Game(canvas.width, canvas.height);
 
   let lastTime = 0;
 
-  // animation loop
-  function animate(timeStamp) {
-
-    if (game.gameOver) {
-      ctx.fillStyle = 'rgba(0,0,0,0.5)';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = 'white';
-      ctx.font = '50px Arial';
-      ctx.fillText('GAME OVER', canvas.width / 2 - 150, canvas.height / 2);
-      return;
-    }
-
-    if (game.score >= game.winningScore) {
-      ctx.fillStyle = 'rgba(0,0,0,0.5)';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = 'white';
-      ctx.font = '50px Arial';
-      ctx.fillText('LEVEL COMPLETE', canvas.width / 2 - 200, canvas.height / 2);
-      return;
-    }
-
-    const deltaTime = timeStamp - lastTime;
-    // console.log(deltaTime);
-
-    lastTime = timeStamp;
-
-    // clear what has already been drawn while looping
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    game.draw(ctx);
-    game.update(deltaTime);
-
-    // using parent as argument to make endless loop
-    requestAnimationFrame(animate);
+  function startGame() {
+    startScreen.style.display = 'none';
+    canvas.style.display = 'block';
+    game.start();
+    animate(0);
   }
 
-  animate(0);
+  startButton.addEventListener('click', startGame);
+
+  function restartGame() {
+    gameOverScreen.style.display = 'none';
+    lastTime = 0;
+    game.reset();
+    startGame();
+  }
+
+  restartButton.addEventListener('click', restartGame);
+  
+  function animate(timeStamp) {
+    if (game.gameOver) {
+      gameOverScreen.style.display = 'block';
+      return; // Stop the animation loop when the game is over.
+    }
+    const deltaTime = timeStamp - lastTime;
+
+    // lastTime = timeStamp;
+    lastTime = performance.now();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update(deltaTime);
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
 });
